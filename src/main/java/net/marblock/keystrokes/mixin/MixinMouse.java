9c1a71b2b0213dda0ae945a633bbcf04db33c4b4
Arsrillon <papapapa1990papa@gmail.com>
2026-02-17 12:48:41 +0200

    build: ported to 1.21.8

--- src/main/java/net/marblock/keystrokes/mixin/MixinMouse.java
+++ src/main/java/net/marblock/keystrokes/mixin/MixinMouse.java
@@ -2,7 +2,6 @@ package net.marblock.keystrokes.mixin;
 
 import net.marblock.keystrokes.ModernKeystrokesClient;
 import net.minecraft.client.MouseHandler;
-import net.minecraft.client.input.MouseButtonInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,16 +9,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MouseHandler.class)
 public class MixinMouse {
-    @Inject(method = "onButton", at = @At("HEAD"))
-    private void onMouseClick(long window, MouseButtonInfo mouseButtonInfo, int action, CallbackInfo ci) {
+    @Inject(method = "onPress", at = @At("HEAD"))
+    private void onMouseClick(long window, int button, int action, int modifiers, CallbackInfo ci) {
         if (action == 1) {
             if (ModernKeystrokesClient.renderer != null) {
-                int button = mouseButtonInfo.button();
-
                 if (button == 0) {
-                    net.marblock.keystrokes.ModernKeystrokesClient.renderer.incrementLeftClicks();
+                    ModernKeystrokesClient.renderer.incrementLeftClicks();
                 } else if (button == 1) {
-                    net.marblock.keystrokes.ModernKeystrokesClient.renderer.incrementRightClicks();
+                    ModernKeystrokesClient.renderer.incrementRightClicks();
                 }
             }
         }
